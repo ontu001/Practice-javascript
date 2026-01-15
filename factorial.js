e1dbdf37e08d8929fb7af99fba3b8afa2540c48d
ontu001 <mdrohanurrahmanontu@gmail.com>
2026-01-15 21:53:07 +0600

    implimented using function

--- factorial.js
+++ factorial.js
@@ -1,6 +1,12 @@
 let a = parseInt(prompt("Enter Your Number = "));
-let fact= 1;
-for(i =1; i<=a; i++){
+
+
+function factorial(a){
+    let fact= 1;
+   for(i =1; i<=a; i++){
     fact = fact*i
 }
 console.log(`Factorial of ${a} is = ${fact}`)
+}
+
+factorial(a)
